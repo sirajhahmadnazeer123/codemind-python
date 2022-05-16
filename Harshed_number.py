043416eca5a7f102803caa0a11ec1fd522490d34
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-05-16 19:53:07 +0530

    num = int(input()) rem = sum = 0;
    n = num;    
    while(num > 0):    
        rem = num%10    
        sum = sum + rem    
        num = num//10    
         
    if(n%sum == 0):    
        print(True)    
    else:    
        print(False)

---
+++ Harshed_number.py
@@ -0,0 +1,13 @@
+num = int(input())    
+rem = sum = 0;    
+     
+n = num;    
+while(num > 0):    
+    rem = num%10    
+    sum = sum + rem    
+    num = num//10    
+     
+if(n%sum == 0):    
+    print(True)    
+else:    
+    print(False)   
