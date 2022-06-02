69aa3c8b734e463d5be6123a35f1bfa12bad2e57
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-06-02 20:48:44 +0530

    min = int(input()) max = int(input()) for n in range(min,max + 1):    if n > 1:        for i in range(2,n):            if (n % i) == 0:                break        else:            print(n)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,10 +1,9 @@
-lower_value = int(input ())  
-upper_value = int(input ())  
-  
-for number in range (lower_value, upper_value + 1):  
-    if number > 1:  
-        for i in range (2, number):  
-            if (number % i) == 0:  
-                break  
-        else:  
-            print (number)  
+min = int(input())
+max = int(input())
+for n in range(min,max + 1):
+   if n > 1:
+       for i in range(2,n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)
