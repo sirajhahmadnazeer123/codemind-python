d2c48e46caf06269c40edcbb850c93be33ccab90
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-06-22 15:26:03 +0530

    n=int(input()) l=list(map(int,input().split())) s=0 g=0 for i in range(0,len(l)):     if i%2==0:         s+=l[i] for i in range(0,len(l)):     if i%2!=0:         g+=l[i] print(abs(s-g))

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=0
+g=0
+for i in range(0,len(l)):
+    if i%2==0:
+        s+=l[i]
+for i in range(0,len(l)):
+    if i%2!=0:
+        g+=l[i]
+print(abs(s-g))
+        
