3fd5b89f7165765beb91c7e4ad9c156d75237622
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-06-29 20:54:21 +0530

    from math import* a=int(input()) for i in range(1,int(sqrt(a)+1)):     c=0     if i*i==a:         c=1 if c==1:     print(True) else:     print(False)

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,8 +1,10 @@
-import math
-number = int(input())
-
-root = math.sqrt(number)
-if int(root + 0.5) ** 2 == number:
-    print("True")
+from math import*
+a=int(input())
+for i in range(1,int(sqrt(a)+1)):
+    c=0
+    if i*i==a:
+        c=1
+if c==1:
+    print(True)
 else:
-    print("False")
+    print(False)
