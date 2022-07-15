1c5e8059bd620ba66b9f7da5c4be32272a00eff4
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-07-15 14:58:11 +0530

    n=int(input()) x=list(map(int,input().split())) s=[] f=0 for i in x:     if i not in s:         s.append(i) for i in s:     if x.count(i)==i:         print(i,end=" ")         f=1 if(f==0):     print("-1")

---
+++ Elements_same_as_count.py
@@ -0,0 +1,13 @@
+n=int(input())
+x=list(map(int,input().split()))
+s=[]
+f=0
+for i in x:
+    if i not in s:
+        s.append(i)
+for i in s:
+    if x.count(i)==i:
+        print(i,end=" ")
+        f=1
+if(f==0):
+    print("-1")
