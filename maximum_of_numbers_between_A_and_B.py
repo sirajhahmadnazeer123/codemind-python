4d016c356a3703f5a9b3254046401e57981956e3
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-07-15 14:50:07 +0530

    n=int(input()) x=list(map(int,input().split())) a,b=map(int,input().split()) s=[] for i in x:     if i in range(a,b+1):         s.append(i) if len(s)==0:     print("-1") else:     print(max(s))

---
+++ maximum_of_numbers_between_A_and_B.py
@@ -0,0 +1,11 @@
+n=int(input())
+x=list(map(int,input().split()))
+a,b=map(int,input().split())
+s=[]
+for i in x:
+    if i in range(a,b+1):
+        s.append(i)
+if len(s)==0:
+    print("-1")
+else:
+    print(max(s))
