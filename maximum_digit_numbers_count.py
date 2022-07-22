1ff85aace0faf2c4ad1e2d483f42cff68a01317b
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-07-22 15:48:24 +0530

    n=int(input()) c=input() s=[] d=str(c) for i in c.split():     s.append(len(i)) x=list(c.split()) for i in x:     if len(i)==max(s):         print(i,end=" ")

--- maximum_digit_numbers_count.py
+++ maximum_digit_numbers_count.py
@@ -1,18 +1,10 @@
-def dig(n):
-    if n==0:
-        return 1
-    c=0
-    if n<0:
-        n=-1*n
-    while n:
-        n=n//10
-        c+=1
-    return c
 n=int(input())
-a=list(map(int,input().split()))
-l=[]
-for i in range(n):
-    l.append(dig(a[i]))
-for i in range(n):
-    if dig(a[i])==max(l):
-        print(a[i],end=' ')
+c=input()
+s=[]
+d=str(c)
+for i in c.split():
+    s.append(len(i))
+x=list(c.split())
+for i in x:
+    if len(i)==max(s):
+        print(i,end=" ")
