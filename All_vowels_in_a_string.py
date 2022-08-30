9199e5c2566cba07e73a41ab99d4edbe39cc44af
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-08-30 12:08:05 +0530

    n=input() k="aeiou" j="AEIOU" f=[] l=[] for i in n:     if i in k:         f.append(i) for i in n:     if i in j:         l.append(i) k=[] s=[] for i in f:     if i not in k:         k.append(i) for i in l:     if i not in s:         s.append(i) if len(k)==5 or len(s)==5:     print("True") else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,11 +1,23 @@
 n=input()
-v="aeiou"
-s=[]
+k="aeiou"
+j="AEIOU"
+f=[]
+l=[]
 for i in n:
-    if i in v:
-        if i not in s:
-            s.append(i)
-if len(s)==len(v):
+    if i in k:
+        f.append(i)
+for i in n:
+    if i in j:
+        l.append(i)
+k=[]
+s=[]
+for i in f:
+    if i not in k:
+        k.append(i)
+for i in l:
+    if i not in s:
+        s.append(i)
+if len(k)==5 or len(s)==5:
     print("True")
 else:
     print("False")
