934aa621cf5b072607e1e4ebfbf0fbea9cd55efc
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-08-30 11:15:00 +0530

    def dig(n):     c=0     while n:         i=n%10         n=n//10         c+=1     return c n=int(input()) a=list(map(int,input().split())) l=[] for i in range(n):     l.append(dig(a[i])) x=min(l) print(l.count(x))

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,14 +1,13 @@
 def dig(n):
-    if n==0:
-        return 1
     c=0
-    while(n!=0):
+    while n:
+        i=n%10
         n=n//10
         c+=1
     return c
 n=int(input())
-l=[]
 a=list(map(int,input().split()))
+l=[]
 for i in range(n):
     l.append(dig(a[i]))
 x=min(l)
