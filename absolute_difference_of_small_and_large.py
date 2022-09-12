b0c4c5eb7240d88d86549da94b83a4737cbcf1e5
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-12 10:39:27 +0530

    n=input() l=[] o=[] k=[] for i in n.split():     l.append(i) for i in l:     o.append(ord(max(i))) for i in l:     k.append(ord(min(i))) for i in range(0,len(o)):     print(o[i]-k[i],end=" ")

--- absolute_difference_of_small_and_large.py
+++ absolute_difference_of_small_and_large.py
@@ -1,6 +1,12 @@
 n=input()
-s=[]
+l=[]
+o=[]
+k=[]
 for i in n.split():
-    s.append(i)
-for i in s:
-    print(abs(ord(min(i))-ord(max(i))),end=" ")
+    l.append(i)
+for i in l:
+    o.append(ord(max(i)))
+for i in l:
+    k.append(ord(min(i)))
+for i in range(0,len(o)):
+    print(o[i]-k[i],end=" ")
