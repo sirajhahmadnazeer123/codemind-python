e52793fccfc5a09ff47b5706b2ae84bfa4033097
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-12 10:10:04 +0530

    n=input() s=[] for i in n.split():     s.append(i) for i in s:     print(min(i),end=" ")     break for i in s[::-1]:     print(max(i))     break

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -2,10 +2,9 @@ n=input()
 s=[]
 for i in n.split():
     s.append(i)
-a=[]
-d=[]
-for i in s[0]:
-    a.append(i)
-for i in s[-1]:
-    d.append(i)
-print(min(a),max(d))
+for i in s:
+    print(min(i),end=" ")
+    break
+for i in s[::-1]:
+    print(max(i))
+    break
