04cbe9a6cf7d72e9dca1dea6318dbcc74a8d9b09
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-12 09:40:37 +0530

    n=input() s=n.lower() e=[] k='' d="abcdefghijklmnopqrstuvwxyz" for i in s:     if s.count(i)==1:         if i in d:             e.append(i) p=set(e) j=list(p) j.sort() for i in j:     k+=i print(k)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -4,8 +4,9 @@ e=[]
 k=''
 d="abcdefghijklmnopqrstuvwxyz"
 for i in s:
-    if i in d:
-        e.append(i)
+    if s.count(i)==1:
+        if i in d:
+            e.append(i)
 p=set(e)
 j=list(p)
 j.sort()
